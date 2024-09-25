31e72e4c40fe6d5e2e5226ada09ba253148d5390
cybrsucks <cybrs36@gmail.com>
2024-09-25 10:38:31 +1000

    heatmap working

--- public/map.js
+++ public/map.js
@@ -8,6 +8,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker to the map
-L.marker([-27.4705, 153.0260]).addTo(map)
+L.marker([-27.4705, 153.0260])
+    .addTo(map)
     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     .openPopup();
+
+
+// Example heatmap data (latitude, longitude, intensity)
+var heatData = [
+    [-27.4705, 153.0260, 1.2], // Example point
+    [-25, 155, 0.7], // Example point
+];
+
+// Create the heat layer
+var heat = L.heatLayer(heatData, {
+    radius: 25,
+    blur: 15,
+    maxZoom: 10
+}).addTo(map);
