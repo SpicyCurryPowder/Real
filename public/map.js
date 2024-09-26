eba64b726a7ff2594dfb46cdc8e286abf625dde9
leevjo <47852192+cybrsucks@users.noreply.github.com>
2024-09-26 23:10:56 +1000

    kool = cabbage update

--- public/map.js
+++ public/map.js
@@ -8,21 +8,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker to the map
-L.marker([-27.4705, 153.0260])
-    .addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+// L.marker([-27.4705, 153.0260])
+//     .addTo(map)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
 
 
 // Example heatmap data (latitude, longitude, intensity)
 var heatData = [
-    [-27.4705, 153.0260, 1.2], // Example point
-    [-25, 155, 0.7], // Example point
+    [-27.4705, 153.026, 0.9], // Example point
+    // [-27.48552, 152.993492, 0.7], // Example point
+    [-27.4977, 153.0129, 0.6]
 ];
 
 // Create the heat layer
 var heat = L.heatLayer(heatData, {
+    radius: 40,
+    blur: 30,
+    maxZoom: 2
+}).addTo(map);
+
+L.heatLayer([[-27.48552, 152.993492, 0.7]], {
     radius: 25,
-    blur: 15,
+    blur: 20,
     maxZoom: 10
 }).addTo(map);
